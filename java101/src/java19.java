5e205824dc8e536aa11fd0a839650c07ed1c0f56
erdemkaramisir <erdemkaramisir@gmail.com>
2023-03-30 16:40:43 +0300

    km hesaplama

---
+++ java101/src/java19.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class java19 {
+    public static void main(String[] args) {
+
+        int km;
+        double perKm = 2.20, total=0, startPrice = 10;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print(" Mesafeyi KM cinsinden giriniz ");
+        km = scan.nextInt();
+
+
+        total += (km * perKm);
+        total += startPrice;
+
+        total = (total < 20) ? 20 : total;
+
+        System.out.println("Toplam tutar :" + total);
+
+    }
+}
