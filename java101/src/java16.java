7e985ecaf26934d1a84cfe0d33e5c1c94a15221a
erdemkaramisir <erdemkaramisir@gmail.com>
2023-03-30 16:18:35 +0300

    second

--- java101/src/java16.java
+++ java101/src/java16.java
@@ -1,14 +1,12 @@
 
-
- 
 import java.util.Scanner;
 
 public class java16 {
     public static void main(String[] args) {
 
-        int mat,fizik, kimya, turkce, tarih, muzik;
+        int mat, fizik, kimya, turkce, tarih, muzik;
 
-        Scanner scan= new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.println(" Matematik Notunuz :");
         mat = scan.nextInt();
@@ -28,19 +26,13 @@ public class java16 {
         System.out.println(" Müzik Notunuz :");
         muzik = scan.nextInt();
 
-        int toplam= (mat + fizik + kimya + turkce + tarih + muzik);
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
 
-        double sonuc= (toplam/6);
+        double sonuc = (toplam / 6);
 
         System.out.println(" Ortalamanız  :" + sonuc);
 
-        if( sonuc>= 50) {
-            System.out.println(" Geçtiniz");
-        } else{
-                System.out.println(" Kaldınız ");
-            }
+        System.out.println(sonuc >= 60 ? "Sınıfı Geçtiniz" : "Sınıfta Kaldınız");
 
-        }
     }
- 
-
+}
--- java101/src/java17.java
+++ java101/src/java17.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class java17 {
+import java.util.Scanner;
+
+public class java17 {
+    public static void main(String[] args) {
+
+        double tutar, kdvOran = 0.18;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print(" Ücret tutarını Giriniz  :");
+        tutar = scan.nextDouble();
+
+        double kdvTutar = tutar * kdvOran;
+
+        double kdvliTutar = tutar + kdvTutar;
+
+        System.out.println(" Hesapladığımız kdv tutarı :" + kdvTutar);
+        System.out.println(" Kdv siz Tutar :" + tutar);
+        System.out.println(" Kdv li tutar :" + kdvliTutar);
+
+    }
 }
