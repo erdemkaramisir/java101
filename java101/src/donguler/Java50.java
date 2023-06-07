a97e837cbd4fba6da8b9c672059f705bbf891de1
erdemkaramisir <erdemkaramisir@gmail.com>
2023-06-07 17:14:19 +0300

    Ters Yıldız

---
+++ java101/src/donguler/Java50.java
@@ -0,0 +1,21 @@
+package donguler;
+import java.util.Scanner;
+
+public class Java50 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n;
+        System.out.println("Basamak sayısını giriniz: ");
+        n = scan.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
