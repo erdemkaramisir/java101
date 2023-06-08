9d5596d60b353fabe7a8970f8c2da73f1a99a86d
erdemkaramisir <erdemkaramisir@gmail.com>
2023-06-08 18:58:44 +0300

    Palindrom sayı bulma

---
+++ java101/src/metotlar/Java53.java
@@ -0,0 +1,33 @@
+package metotlar;
+
+import java.util.Scanner;
+
+public class Java53 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int number = scanner.nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " bir palindrom sayıdır.");
+        } else {
+            System.out.println(number + " bir palindrom sayı değildir.");
+        }
+    }
+
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reverse = 0;
+
+        while (number != 0) {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number /= 10;
+        }
+
+        return originalNumber == reverse;
+    }
+}
+
+    
